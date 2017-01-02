384565603fc65b0fd34a34154acb0a43bab4077d
Brandon Cheng <gluxon@users.noreply.github.com>
2017-01-01 20:41:14 -0500

    Allow start script to be ran from anywhere

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-docker-compose run --rm personalshell
+
+# Get full path of script
+# http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P ) 
+
+docker-compose -f $SCRIPTPATH/docker-compose.yml run --rm personalshell
